7560c207968c235bc3449d0db3f9d404bb47c385
NandreyN <nandreyn1998@gmail.com>
2017-02-21 21:09:07 +0300

    3 tasks

--- Sem2Lab3/main.cpp
+++ Sem2Lab3/main.cpp
@@ -18,6 +18,22 @@ void split(const string& str, vector<string>& target)
 	copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(target));
 }
 
+ostream& dline(ostream& o)
+{
+	o << "/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////" << endl;
+	return o;
+}
+
+set<string> getUnion(const vector<set<string>>& setsCollection)
+{
+	set<string> result;
+	for_each(setsCollection.begin(), setsCollection.end(), [&result](const set<string>& st)
+	{
+		result.insert(st.begin(), st.end());
+	});
+	return result;
+}
+
 int main()
 {
 	// First string of an input file contains info on all the species of fishes
@@ -26,6 +42,7 @@ int main()
 	set<string> allTypes;
 	vector<string> auxiliary;
 	map<int, vector<string>> fishermanData;
+	vector<set<string>> fishermanSets;
 
 	fstream in("in.txt");
 	assert(in.is_open());
@@ -40,6 +57,10 @@ int main()
 		getline(in, data);
 		split(data, auxiliary);
 		fishermanData.insert(make_pair(id, auxiliary));
+
+		set<string> st;
+		copy(auxiliary.begin(), auxiliary.end(), inserter(st, st.begin()));
+		fishermanSets.push_back(st);
 		auxiliary.clear();
 	}
 
@@ -50,11 +71,22 @@ int main()
 	// Each fisherman has ...
 	for_each(fishermanData.begin(), fishermanData.end(), [&out](pair<int, vector<string>> pair)
 	{
-		out << "Fisherman" << pair.first << " : "; 
+		out << "Fisherman" << pair.first << " has : "; 
 		copy(pair.second.begin(), pair.second.end(), ostream_iterator<string>(out, "  ")); 
 		out << endl;
 	});
+	out << dline;
 
+
+	set<string> fUnion = getUnion(fishermanSets);
+	copy(fUnion.begin(), fUnion.end(), ostream_iterator<string>(out, " "));
+	out << endl << dline;
+
+
+	set<string> diff;
+	set_difference(allTypes.begin(), allTypes.end(), fUnion.begin(), fUnion.end(), inserter(diff, diff.begin()));
+	copy(diff.begin(), diff.end(), ostream_iterator<string>(out, " "));
+	out << endl << dline;
 	out.close();
 	return 0;
 }
